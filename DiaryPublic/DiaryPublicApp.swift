9da365cb63d4df499a4d70fa38bf2ad737e0c018
404ibra <ibra.aktass@gmail.com>
2023-04-09 22:16:52 +0300

    Initial Commit

---
+++ DiaryPublic/DiaryPublicApp.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryPublicApp.swift
+//  DiaryPublic
+//
+//  Created by İbrahim Aktaş on 9.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiaryPublicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
